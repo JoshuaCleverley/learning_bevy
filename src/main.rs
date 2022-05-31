02670e13e92c7acc6cc3e6bf163036e090d018bd
JoshuaCleverley <joshuacleverley@protonmail.com>
2022-05-31 17:57:55 +0100

    First components and systems

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,30 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
+        .add_startup_system(add_people)
         .add_system(hello_world)
+        .add_system(greet_people)
         .run();
 }
 
-// First system!
 fn hello_world() {
     println!("Hello, world!");
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
+    commands.spawn().insert(Person).insert(Name("Renzo Hume".to_string()));
+    commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("Hello, {}", name.0);
+    }
 }

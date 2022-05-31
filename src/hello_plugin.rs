b1893f777361a1c4643cd2deae2060ecacb474ac
JoshuaCleverley <joshuacleverley@protonmail.com>
2022-05-31 18:10:18 +0100

    Timer resource

--- src/hello_plugin.rs
+++ src/hello_plugin.rs
@@ -5,14 +5,10 @@ pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
         app
+            .insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
             .add_startup_system(add_people)
-            .add_system(hello_world)
             .add_system(greet_people);
     }
-}
-
-fn hello_world() {
-    println!("Hello, world!");
 }
 
 #[derive(Component)]
@@ -27,8 +23,16 @@ fn add_people(mut commands: Commands) {
     commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("Hello, {}", name.0);
-    }
+struct GreetTimer(Timer);
+
+fn greet_people(
+    time: Res<Time>,
+    mut timer: ResMut<GreetTimer>,
+    query: Query<&Name, With<Person>>
+) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("Hello, {}", name.0);
+        }
+    } 
 }
